f5954386acf69173b8f8d9798c1801941f182514
Con <ckritikos@gmail.com>
2024-06-25 08:05:01 +1000

    Clean up code slightly

--- main.py
+++ main.py
@@ -5,15 +5,18 @@ scr = Screen()
 # create line
 line = Turtle()
 
-# screen attributes
-SCREEN_WIDTH = 1000
-SCREEN_HEIGHT = 550
+# screen constants
+SCREEN_WIDTH = 900
+SCREEN_HEIGHT = 600
 SCREEN_COLOUR = "black"
+SCREEN_TITLE = "Pong Game"
+
+# screen setup
 scr.setup(width=SCREEN_WIDTH, height=SCREEN_HEIGHT)
 scr.colormode(255)
 scr.bgcolor(SCREEN_COLOUR)
 
-#line attributes
+# line attributes
 line.setpos(x=0, y=int(SCREEN_HEIGHT/2))
 line.hideturtle()
 line.shapesize(10)
